7a0912da5582d2a2ca7c03055f9d5fef4b87bbe8
YukiHSun <Yuki_Hamaguchi@suntory.co.jp>
2024-11-13 12:19:19 +0900

    modify pred.py

--- python_zip_code/regression/pred.py
+++ python_zip_code/regression/pred.py
@@ -1,25 +1,13 @@
 import numpy as np
 import pandas as pd
-import os
+from pathlib import Path
 from pycaret.regression import load_model
 
-# Specify the model file name
-MODEL_FILE_NAME = "best_model_weight_pounds.pkl"
+# Specify the model file path
+MODEL_PATH = Path(__file__).absolute().parent / "best_model_weight_pounds.pkl"
 
-# Load the model file from the current execution directory
-current_directory = os.path.dirname(__file__)
-model_path = os.path.join(current_directory, MODEL_FILE_NAME)
-
-# Remove the extension (since load_model automatically appends it)
-if model_path.endswith('.pkl'):
-    model_path = model_path[:-4]  # Remove the '.pkl' at the end
-
-# Check if the file exists
-if not os.path.exists(model_path + ".pkl"):
-    raise FileNotFoundError(f"Model file not found at {model_path}.pkl")
-
-# Load the model
-model = load_model(model_path)
+# Load the model (load_model automatically appends the necessary suffix)
+model = load_model(MODEL_PATH.with_suffix(""))
 
 def predict(inputs: pd.DataFrame) -> np.ndarray:
     """
